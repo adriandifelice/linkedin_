dd02157f8a7aa791516b0c5d529cf8b5c1d4ed0e
Adrian Di Felice <adrian.di.felice@gmail.com>
2021-03-17 12:41:28 -0400

    fixed result side

--- client/src/components/Results.js
+++ client/src/components/Results.js
@@ -2,15 +2,12 @@ import React from 'react';
 import '../styles/App.css'
 
 const Results = ({results, number}) => {
- const searchItems = results.items;
  const num = number;
 
- const limitedRes = searchItems.slice(0, number);
 
   return ( 
     <>
-      <h1>Results</h1>
-      {limitedRes? limitedRes.map(result => {return <div className='result'>
+      {results? results.items.slice(0,num).map(result => {return <div className='result'>
         <h3>{result.title}</h3>
         <p>{result.snippet}</p>
       </div>} ):'No Items'}
